7629c9a53f545eea4b5939e711d643605815487f
DisDoode <skaathungarm@gmail.com>
2025-05-23 22:16:41 +0200

    Describe your changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Ordbok from './Ordbok';
 import { Box } from '@mui/material';
 import { Link } from "react-router-dom";
 
+
 function App() {
     return (
         <div className="App">
@@ -37,52 +38,43 @@ function App() {
                     </Box>
                 </Grid>
             </Grid>
-
-            <BrowserRouter>
-
+            <BrowserRouter basename="/my-app">
                 <Grid container spacing={2}>
                     <Grid size={2}>
                         <Stack>
-<Button className="menuButton" component={Link} to="/" variant="text">Start <HomeIcon className="menuIcon" /></Button>
-<Button className="menuButton" component={Link} to="/Kul" variant="text">Logg <ArticleIcon className="menuIcon" /></Button>
-<Button className="menuButton" component={Link} to="/Jobb" variant="text">Om Jobb <WorkIcon className="menuIcon" /></Button>
-<Button className="menuButton" component={Link} to="/Spill" variant="text">Spill <SportsEsportsIcon className="menuIcon" /></Button>
-<Button className="menuButton" component={Link} to="/Cat" variant="text">Cat <PetsIcon className="menuIcon" /></Button>
-<Button className="menuButton" component={Link} to="/Ordbok" variant="text">Ordbok <MenuBookIcon className="menuIcon" /></Button>
+                            <Button className="menuButton" component={Link} to="/" variant="text">Start <HomeIcon className="menuIcon" /></Button>
+                            <Button className="menuButton" component={Link} to="/Kul" variant="text">Logg <ArticleIcon className="menuIcon" /></Button>
+                            <Button className="menuButton" component={Link} to="/Jobb" variant="text">Om Jobb <WorkIcon className="menuIcon" /></Button>
+                            <Button className="menuButton" component={Link} to="/Spill" variant="text">Spill <SportsEsportsIcon className="menuIcon" /></Button>
+                            <Button className="menuButton" component={Link} to="/Cat" variant="text">Cat <PetsIcon className="menuIcon" /></Button>
+                            <Button className="menuButton" component={Link} to="/Ordbok" variant="text">Ordbok <MenuBookIcon className="menuIcon" /></Button>
                             <Box sx={{ border: "1px solid white", paddingLeft: "40px", height: "95vh", borderRadius: "10px", backgroundColor: '#00004d', color: "white", }}></Box>
                         </Stack>
                     </Grid>
-
                     <Grid size={10}>
                         <Routes>
-                      
-                                <Route index element={<Home />} />
-                                <Route path="/Kul" element={<Kul />} />
-                                <Route path="/Jobb" element={<Jobb />} />
-                                <Route path="/Spill" element={<Spill />} />
-                                <Route path="/Cat" element={<Cat />} />
-                                <Route path="/Ordbok" element={<Ordbok />} />
-                
+                            <Route index element={<Home />} />
+                            <Route path="/Kul" element={<Kul />} />
+                            <Route path="/Jobb" element={<Jobb />} />
+                            <Route path="/Spill" element={<Spill />} />
+                            <Route path="/Cat" element={<Cat />} />
+                            <Route path="/Ordbok" element={<Ordbok />} />
                         </Routes>
                     </Grid>
                 </Grid>
-
                 <Box className="BoxUnder">
                     <Grid container spacing={2}>
                         <Grid size={5}>
                             <p className="strokemeSmall" style={{ fontSize: '30px' }}>Kodet av:</p>
-                            <Box className="item" >    <p className="strokemeTiny">Garm</p></Box>
+                            <Box className="item"><p className="strokemeTiny">Garm</p></Box>
                         </Grid>
                         <Grid size={2}>
-
-                            <p className="strokemeSmall" >Kontakt</p>
+                            <p className="strokemeSmall">Kontakt</p>
                             <Box className="item"><p className="strokemeTiny">Nei takk</p></Box>
                         </Grid>
-
                         <Grid size={4}>
-
-                            <p className="strokemeSmall" >Følg meg</p>
-                            <Box className="item">   <a className="strokemeTiny" href="https://scratch.mit.edu/users/dis_doode/" >dis_doode på scratch</a></Box>
+                            <p className="strokemeSmall">Følg meg</p>
+                            <Box className="item"><a className="strokemeTiny" href="https://scratch.mit.edu/users/dis_doode/">dis_doode på scratch</a></Box>
                         </Grid>
                     </Grid>
                 </Box>
@@ -91,4 +83,4 @@ function App() {
     );
 }
 
-export default App; 
+export default App;
